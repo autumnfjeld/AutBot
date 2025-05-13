ad398fe0b1253d095efa4a0f81060b009f4a3081
Autumn Fjeld <autumn.fjeld@gmail.com>
2025-05-13 19:21:19 +1000

    Use ES Modules.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
