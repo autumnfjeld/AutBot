c66aa774796c21790f0142c8481f86cfb4637dd4
Autumn Fjeld <autumn.fjeld@gmail.com>
2025-05-16 16:25:03 +1000

    Use index.css, play with colors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import autLogo from './assets/aut-sticker.jpg'
-import './App.css'
+import autLogo from './assets/autbot-2.png'
+import './index.css'
 
 function App() {
   const [inputValue, setInputValue] = useState('')
@@ -19,7 +19,7 @@ function App() {
     setError('')
     
     try {
-      const res = await fetch('http://localhost:3000/api/query', {
+      const res = await fetch('http://localhost:3001/api/query', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -42,27 +42,27 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-stone-200 p-6">
+      <h1 className="text-4xl font-heading mb-6 text-sky-700">The Aut Bot</h1>
       <div className="flex space-x-4 mb-6">
         <a href="http://autumnfjeld.com" target="_blank">
-          <img src={autLogo} className="h-12" alt="AutBot logo" />
+          <img src={autLogo} className="h-48 rounded-full border-3 border-lime-700" alt="AutBot logo" />
         </a>
       </div>
-      <h1 className="text-4xl font-bold mb-6 text-indigo-900">Aut Bot</h1>
-      <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
+      <div className="bg-magnolia shadow-md rounded-lg p-6 w-full max-w-md">
         <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
           <input
             type="text"
             value={inputValue}
             onChange={handleInputChange}
-            placeholder="Ask aut anything..."
-            className="p-3 border rounded-md text-lg"
+            placeholder="Ask the AutBot..."
+            className="p-3 border-2 border-stone-400 rounded-md text-md"
           />
           <button 
             type="submit"
             disabled={isLoading || !inputValue.trim()}
             className={`p-3 text-white text-lg font-medium rounded-md ${
-              isLoading ? 'bg-gray-400 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700'
+              isLoading ? ' cursor-not-allowed' : 'bg-lime-700 hover:bg-lime-900'
             }`}
           >
             {isLoading ? 'Thinking...' : 'Ask'}
@@ -76,10 +76,11 @@ function App() {
         {response && (
           <div className="mt-6 p-4 bg-indigo-100 rounded-md">
             <p>{response}</p>
-          </div>
+          </div> 
         )}
       </div>
-      <p className="mt-8 text-gray-500">Ask aut anything.</p>
+      <p className="mt-8 text-green-700"></p>
+      <a href="https://github.com/autumnfjeld/AutBot" target="_blank" className="text-sky-700 hover:text-blue-700">github.com/autumnfjeld/AutBot</a>
     </div>
   )
 }
