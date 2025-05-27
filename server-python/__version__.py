ac8ec9a2067a6d68aeae983da98cec523b79f415
Autumn Fjeld <autumn.fjeld@gmail.com>
2025-05-27 16:10:10 +1000

    Update both client and python server version.

--- server-python/__version__.py
+++ server-python/__version__.py
@@ -2,12 +2,12 @@
 AutBot Server Version Information
 """
 
-__version__ = "0.1.0"
-__version_info__ = (0, 1, 0)
+__version__ = "0.2.0"
+__version_info__ = (0, 2, 0)
 
 # Version history
 CHANGELOG = {
-
+    "0.2.0": "Added query and response logging and a daily cron job to make daily local copies of heroku logs.",
     "0.1.0": "Working version with document weighting.",
     # Future versions will be added here
 } 
